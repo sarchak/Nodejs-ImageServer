0c3d23a6d160a6284647bc853f4d7a8e9e2d208f
Shrikar Archak <shrikar84@gmail.com>
2012-01-04 23:03:31 -0800

    Fix code for deploying onto heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require("http");
 var url  = require("url");
 var formidable = require('formidable');
-
+var port = process.env.PORT || 8000;
 function start(route, handle){
 	function onrequest(request,response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for pathname :" + pathname);
 		route(handle,pathname,request,response);
 	}
-	http.createServer(onrequest).listen(8000);
-	console.log("Server listening on port 8000");
+	http.createServer(onrequest).listen(port);
+	console.log("Server listening on port "+port);
 }
 exports.start = start;
